690c4cfa5c278cf6bded164adb7aebb2ae4dc908
Xanlite <mathderoover@gmail.com>
2018-04-19 17:42:37 +0200

    Adding idealchannel_exec

--- LDPC_testScript.m
+++ LDPC_testScript.m
@@ -11,24 +11,28 @@ H = [   1 1 0 1 1 0 0 1 0 0;        % Define Parity Check matrix H:
         0 0 1 0 0 1 0 1 0 1;   ];   %
 
 %% Step 1
-% N = 1e2;
-% bps = 4;
-% bitStream = CreateBitStream(N,bps);
-% [bitStream_enc,newH] = LDPC_encoder_lite( bitStream, H );
-% % noise = rand(1,numel(bitStream_enc))'>0.99;
-% 
-% % bitStream_enc'
-% 
-% % bitStream_enc = mod(bitStream_enc+noise,2);
-% bitStream_enc(5:10:end) = ~bitStream_enc(5:10:end);         % Add some biterrors manually
-% 
-% tic
-% % bitStream_rec = LDPC_decoder_hard( bitStream_enc, newH );
-% toc
-% 
+N = 1e2;
+bps = 4;
+bitStream = CreateBitStream(N,bps);
+[bitStream_enc,newH] = LDPC_encoder_lite( bitStream, H );
+% noise = rand(1,numel(bitStream_enc))'>0.99;
+
+% bitStream_enc'
+
+% bitStream_enc = mod(bitStream_enc+noise,2);
+bitStream_enc(5:10:end) = ~bitStream_enc(5:10:end);         % Add some biterrors manually
+
+%tic
+% bitStream_rec = LDPC_decoder_hard( bitStream_enc, newH );
+%toc
+
 % tic
 % bitStream_rec = LDPC_decoder_hard_lite( bitStream_enc, H );
 % toc
+
+tic
+bitStream_rec = block_decoder( bitStream_enc, H );
+toc
 
 %% Step 2
 % N                           = 50;
@@ -49,21 +53,21 @@ H = [   1 1 0 1 1 0 0 1 0 0;        % Define Parity Check matrix H:
 % toc
 
 %% Step 3
-N                           = 100;
-c_length                    = 5;
-v_length                    = 10;
-bitStream                   = CreateBitStream(N,c_length);
-%bitStream = [1 0 0 1 1]';
-H0                          = makeLdpc(c_length,v_length,0,1,3);
-[bitStream_enc,newH]        = LDPC_encoder_lite( bitStream, H0 ); %Was prev H
-
-bitStream_chan = real(IdealChannel_exec(bitStream_enc,200,'BPSK','no_demodu'));
-figure
-histogram(abs(bitStream_chan),0:0.05:2)
-
-tic
-bitStream_rec = LDPC_decoder_soft_log_BPSK( bitStream_chan,H,std(bitStream_chan(bitStream_chan>0)) );
-toc
+% N                           = 100;
+% c_length                    = 5;
+% v_length                    = 10;
+% bitStream                   = CreateBitStream(N,c_length);
+% %bitStream = [1 0 0 1 1]';
+% H0                          = makeLdpc(c_length,v_length,0,1,3);
+% [bitStream_enc,newH]        = LDPC_encoder_lite( bitStream, H0 ); %Was prev H
+% 
+% bitStream_chan = real(IdealChannel_exec(bitStream_enc,200,'BPSK','no_demodu'));
+% figure
+% histogram(abs(bitStream_chan),0:0.05:2)
+% 
+% tic
+% bitStream_rec = LDPC_decoder_soft_log_BPSK( bitStream_chan,H,std(bitStream_chan(bitStream_chan>0)) );
+% toc
 
 %% Plotting results
 
